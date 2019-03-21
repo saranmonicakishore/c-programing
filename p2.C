b268b43b974ff8b33009b4ea28b63e56993b903c
saranmonicakishore <46959686+saranmonicakishore@users.noreply.github.com>
2019-03-21 16:09:26 +0530

    Create p2.C

---
+++ p2.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+int num,i,fact=1;
+  printf("enter the number");
+  scanf("%d",&num);
+  for(i=1;i<=num;i++)
+  {
+  fact=fact*i;
+  }
+  printf("\n%d",fact);
+  return 0;
+}
